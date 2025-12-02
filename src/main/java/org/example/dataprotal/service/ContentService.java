b068bd57340d9702c8bfcd534f35d97c41cb3b2f
Sadat Mammadly <memedlisedat033@gmail.com>
2025-12-02 22:16:58 +0400

    code updated

--- src/main/java/org/example/dataprotal/service/ContentService.java
+++ src/main/java/org/example/dataprotal/service/ContentService.java
@@ -1,6 +1,7 @@
 package org.example.dataprotal.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.example.dataprotal.dto.ContentDto;
 import org.example.dataprotal.mapper.ContentMapper;
 import org.example.dataprotal.model.page.Content;
@@ -14,9 +15,16 @@ import org.springframework.web.multipart.MultipartFile;
 public class ContentService {
     private final ContentRepository contentRepository;
     private final ContentMapper contentMapper;
+    private final FileService fileService;
 
+    @SneakyThrows
     public Content createContent(ContentDto contentDto, MultipartFile file) {
         Content content= contentMapper.dtoToEntity(contentDto);
+        if(file!=null && !file.isEmpty()) {
+            String img = fileService.uploadFile(file);
+            content.setImgUrl(img);
+        }
+
         return contentRepository.save(content);
     }
 
@@ -25,9 +33,14 @@ public class ContentService {
         return contentRepository.findByPageName(pageName).orElseThrow(()-> new RuntimeException("Content not found"));
     }
 
+    @SneakyThrows
     public Content updateContent(Long id, ContentDto contentDto, MultipartFile file) {
         Content content = contentRepository.findById(id).orElseThrow(()-> new RuntimeException("Content not found"));
         Content updatedContent = contentMapper.updateContentFromDto(contentDto,content);
+        if(file!=null && !file.isEmpty()) {
+            String img = fileService.uploadFile(file);
+            updatedContent.setImgUrl(img);
+        }
         return contentRepository.save(updatedContent);
     }
 
